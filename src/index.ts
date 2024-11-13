356b4e0082e54a76d81bad7592a81b6aa7f39a49
권샘찬/sam <hypemova@gmail.com>
2024-11-13 13:57:16 +0900

    fix: 인터셉터 파일로 분리 후 index.ts에서 export 하도록 수정

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,2 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { map } from "rxjs";
-import ResponseEntity from "./response-entity";
-
-export default class ResponseEntityInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler<any>) {
-        const response = context.switchToHttp().getResponse();
-
-        return next.handle().pipe(
-            map((data) => {
-                if (data instanceof ResponseEntity) {
-                    const headers = data.getHeaders.getAll;
-                    const headerKeys = Object.keys(headers);
-                    for (const headerKey of headerKeys) {
-                        const headerValue = headers[headerKey];
-                        response.setHeaders.set(headerKey, headerValue);
-                    }
-                    response.statusCode = data.statusCode;
-                    return data.body;
-                }
-
-                return data;
-            })
-        )
-    }
-
-}
+export * from './response-entity.interceptor';
+export * from './response-entity';
